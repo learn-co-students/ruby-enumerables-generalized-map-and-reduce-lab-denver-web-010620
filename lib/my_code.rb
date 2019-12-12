23fdacca220cb80f91abc12f338d1a11d562beb5
Bradley Haley <bshaley25@gmail.com>
2019-12-12 16:42:06 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
 # Your Code Here
+
+def map(source_array)
+    result = []
+    index = 0 
+    while index < source_array.length
+      result.push(yield(source_array[index])) 
+      index += 1 
+    end
+    result
+end
+
+def reduce(array, sv=nil)
+  if sv
+    sum = sv
+    i = 0 
+  else
+    sum = array[0]
+    i = 1
+  end
+  while i < array.length
+   sum = yield(sum, array[i])
+   i += 1 
+  end
+  sum
+end
