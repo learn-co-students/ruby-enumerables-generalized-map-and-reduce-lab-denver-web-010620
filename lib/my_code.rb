8f4684da12115d456032e5ab503f7c7c8df79c33
Michael Newman <coloradohokie@gmail.com>
2019-12-20 06:11:51 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,21 +11,18 @@ end #method
 
 
 def reduce(array, starting_point=nil)
+  new_array = array
   if starting_point
-    total = starting_point
-    i=0
-    
-  else
-    total = array[0]
-    i=1
+    new_array.unshift(starting_point)
   end
+    
+  total = new_array[0]
+  i=1
   
-  while i < array.length do
-    total = yield(total, array[i])
+  
+  while i < new_array.length do
+    total = yield(total, new_array[i])
     i += 1
   end #while
   return total
 end #method
-    
-
-
